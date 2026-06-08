b6969c257dcd8d8a3d6503fcd73121051cd5fe42
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-06-08 19:13:36 +0200

    init project

---
+++ include/cjson.h
@@ -0,0 +1,7 @@
+#ifndef CJSON_H
+#define CJSON_H
+
+/* This generated file contains includes for project dependencies */
+#include "cjson/bake_config.h"
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,5 @@
+#include <cjson.h>
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
